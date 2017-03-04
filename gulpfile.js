08eb50d6197ffe668519539822bc7a83a201f354
Christine Cha <ccha@squarespace.com>
2017-03-04 16:06:37 -0500

    Fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const path = require('path')
 gulp.task('less', function() {
   const src  = 'styles/src/index.less'
   const path = ['styles/src']
-  const dist = 'styles/dist'
+  const dist = 'assets/styles'
 
   return gulp.src(src)
     .pipe(less({ path }))
