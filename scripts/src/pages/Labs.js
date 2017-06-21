b247a97c45c063c0a36be0e484f9bf6c5b757b04
Christine Cha <hello@christinecha.com>
2017-06-20 23:13:07 -0400

    Cleanup

--- scripts/src/pages/Labs.js
+++ scripts/src/pages/Labs.js
@@ -1,14 +1,6 @@
-const Hammer = require('hammerjs')
-
-const transitionEnd = require('../lib/transition-end')
-
-const BaseGallery = require('../blocks/Gallery')
-const Pagination = require('../blocks/Pagination')
-
-
 class Labs {
   constructor() {
-    this.$labs           = document.getElementById('labs')
+    this.$labs = document.getElementById('labs')
 
     this.addEventListeners()
   }
